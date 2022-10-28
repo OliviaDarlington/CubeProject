f3180c8a2d9d131e5626512d64751f34108cbde2
OliviaDarlington <114582532+OliviaDarlington@users.noreply.github.com>
2022-10-28 14:28:46 +0100

    Environment

--- The Cube Game/Assets/Scripts/Health.cs
+++ The Cube Game/Assets/Scripts/Health.cs
@@ -2,31 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour
-{
+//public class Health : MonoBehaviour
+//{
+// tutorial on yt helped with this, "BMo" is the channel name
+// link to video https://youtu.be/vNL4WYgvwd8
 
-    public int maxHealth = 100;
-    public int currentHealth;
+   //public int maxHealth = 100;
+    //public int currentHealth;
+    
+    //public Animator anim;
 
-   public HealthBar healthBar;
+   //public HealthBar healthBar;
     // Start is called before the first frame update
-    void Start()
-    {
-        currentHealth = maxHealth;
-        healthBar.SetMaxHealth(maxHealth);
-    }
+    //void Start()
+    //{
+        //currentHealth = maxHealth;
+        //healthBar.SetMaxHealth(maxHealth);
+    //}
 
 
-    void TakeDamage(int amount)
-    {
-        currentHealth -= amount;
+   //public void TakeDamage(int amount)
+    //{
+        //currentHealth -= amount;
 
-        healthBar.SetHealth(currentHealth);
+       // healthBar.SetHealth(currentHealth);
 
-        if(currentHealth <= 0)
-        {
+        //if(currentHealth <= 0)
+        //{
           //dead
+          //anim.SetBool("isDead", true);
          
-        }
-    }
-}
+        //}
+
+    //public void Heal(int amount)
+    //{
+        //currentHealth += amount;
+
+        //healthBar.SetHealth(currentHealth);
+
+        //if(currentHealth > maxHealth)
+        //{
+         // currentHealth = maxHealth;
+        //}
+    //}
+    //}
+//}
--- The Cube Game/Assets/Scripts/PlayerMovement.cs
+++ The Cube Game/Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private float walkSpeed;
     [SerializeField] private float runSpeed;
+    
 
     private Vector3 moveDirection;
     private Vector3 velocity;
